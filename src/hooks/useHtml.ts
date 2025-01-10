e574f8413bcee0a33eb84f2a18ea121cf14bafb4
wj <1220253798@qq.com>
2025-01-11 00:25:15 +0800

    clear

--- src/hooks/useHtml.ts
+++
@@ -1,9 +0,0 @@
-export default function useHtml(selector: string, attribute?: string, attributeValue?: any) {
-  const html = document.querySelectorAll(selector)
-  if (attribute)
-    html[0]?.setAttribute(attribute, attributeValue)
-
-  return html[0]
-    ? html.length > 1 ? html : html[0]
-    : null
-}
--- src/pages/home/index.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/home/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/home/"!</div>
-}
